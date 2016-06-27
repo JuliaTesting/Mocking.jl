f583da9dc1d9431bfda3b096221780b2352dc665
Curtis Vogt <curtis.vogt@gmail.com>
2016-06-27 09:44:42 -0500

    Add nested test

---
+++ test/real-nested.jl
@@ -0,0 +1,17 @@
+let
+    readfile(filename) = (@mock isfile(filename)) ? readstring(@mock open(filename)) : ""
+
+    # Testing with both generic and anonymous functions
+    patches = Patch[
+        @patch isfile(f::AbstractString) = f == "foo"
+        @patch open(f::AbstractString) = IOBuffer("bar")
+    ]
+
+    @test readfile("foo") == ""
+
+    apply(patches) do
+        @test readfile("foo") == "bar"
+    end
+
+    @test readfile("foo") == ""
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,10 @@ using Base.Test
 include("concept.jl")
 include("scope.jl")
 include("closure.jl")
+include("import.jl")
 include("real-open.jl")
 include("real-isfile.jl")
-include("import.jl")
+include("real-nested.jl")
 
 # include("basic.jl")
 # include("signature.jl")
