40775fee5901baf21af07adc9df10c98e99a8866
Curtis Vogt <curtis.vogt@gmail.com>
2016-06-27 10:01:32 -0500

    Remove left-overs

--- test/exception.jl
+++
@@ -1,5 +0,0 @@
-import Mocking: FunctionError
-
-buffer = IOBuffer()
-showerror(buffer, FunctionError(Base, :foo))
-@test takebuf_string(buffer) == "FunctionError: function foo does not exist in module Base"
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,11 +8,3 @@ include("import.jl")
 include("real-open.jl")
 include("real-isfile.jl")
 include("real-nested.jl")
-
-# include("basic.jl")
-# include("signature.jl")
-# include("util.jl")
-# include("exception.jl")
-# include("mend.jl")
-
-
