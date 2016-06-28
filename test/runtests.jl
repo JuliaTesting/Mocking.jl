d955233e56eddcc138e920b2065911346653e834
Curtis Vogt <curtis.vogt@gmail.com>
2016-06-27 21:20:24 -0500

    Move import back to global scope

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,15 @@
 # Note: Explicitly setting JULIA_TEST should only be needed on Julia 0.4
-withenv("JULIA_TEST" => 1) do
-    using Mocking
-    using Base.Test
-    import Mocking: apply
+# The environmental variable will only be set for the current Julia session
+ENV["JULIA_TEST"] = 1
 
-    include("concept.jl")
-    include("scope.jl")
-    include("closure.jl")
-    include("import.jl")
-    include("real-open.jl")
-    include("real-isfile.jl")
-    include("real-nested.jl")
-end
+using Mocking
+using Base.Test
+import Mocking: apply
+
+include("concept.jl")
+include("scope.jl")
+include("closure.jl")
+include("import.jl")
+include("real-open.jl")
+include("real-isfile.jl")
+include("real-nested.jl")
