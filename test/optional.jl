ec8b45992c3d928f7957cb71282d6cdd59d986f0
Curtis Vogt <curtis.vogt@gmail.com>
2016-06-29 11:34:28 -0500

    Removing anoymous function limitation tests

--- test/optional.jl
+++ test/optional.jl
@@ -1,18 +1,5 @@
 import Base.Dates: Hour
 
-# Mocking uses makes of anonymous functions for patches. Mocking cannot support optional
-# or keyword parameters while these are unsupported with anonymous functions.
-if VERSION < v"0.5-"
-    # Optional parameters are not allowed in anonymous functions
-    expr = :((x::Integer=0) -> x)
-    @test_throws ErrorException eval(expr)
-
-    # Keyword parameters are not allowed in anonymous functions
-    expr = :((x; debug::Bool=false) -> debug)
-    @test_throws ErrorException eval(expr)
-end
-
-
 # Creating a patch with an optional parameter
 let
     hourvalue(h::Hour=Hour(0)) = Base.Dates.value(h)
