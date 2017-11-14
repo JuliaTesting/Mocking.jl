54d790ca5a24f387f818e7785ea832b7ca66bc1c
Curtis Vogt <curtis.vogt@gmail.com>
2017-11-14 11:14:55 -0600

    Finish renaming precompile (#29)

--- src/options.jl
+++ src/options.jl
@@ -1,19 +1,19 @@
 # Name of the `julia` command-line option
-const COMPILE_MODULES_FLAG = if VERSION >= v"0.7.0-DEV.1698"
+const COMPILED_MODULES_FLAG = if VERSION >= v"0.7.0-DEV.1698"
     Symbol("compiled-modules")
 else
     :compilecache
 end
 
 # Name of the field in the JLOptions structure which corresponds to the command-line option
-const COMPILE_MODULES_FIELD = if VERSION >= v"0.7.0-DEV.1698"
+const COMPILED_MODULES_FIELD = if VERSION >= v"0.7.0-DEV.1698"
     :use_compiled_modules
 else
     :use_compilecache
 end
 
-const DISABLE_COMPILE_MODULES_STR = "--$COMPILE_MODULES_FLAG=no"
-const DISABLE_COMPILE_MODULES_CMD = `$DISABLE_COMPILE_MODULES_STR`
+const DISABLE_COMPILED_MODULES_STR = "--$COMPILED_MODULES_FLAG=no"
+const DISABLE_COMPILED_MODULES_CMD = `$DISABLE_COMPILED_MODULES_STR`
 
 # Generate a mutable version of JLOptions
 let
@@ -27,23 +27,29 @@ let
     end
 end
 
-function is_precompile_enabled()
-    opts = Base.JLOptions()
-    field = COMPILE_MODULES_FIELD
+"""
+    compiled_modules_enabled() -> Bool
 
-    # When the pre-compile field is empty it means pre-compilation is unsupported. If the
-    # pre-compile field is missing that means pre-compilation to be assumed to be enabled.
+Determine if the `julia` command line flag `--$COMPILED_MODULES_FLAG` has been set to "yes".
+"""
+function compiled_modules_enabled()
+    opts = Base.JLOptions()
+    field = COMPILED_MODULES_FIELD
+
+    # When the field is set to `Symbol()` it means that compiled-modules is unsupported.
+    # If the compiled-modules field is undefined we assume that compiled-modules is enabled
+    # by default.
     return field != Symbol() && (!isdefined(opts, field) || Bool(getfield(opts, field)))
 end
 
 """
-    use_precompile(state::Bool) -> Void
+    set_compiled_modules(state::Bool) -> Void
 
-Override the Julia command line flag `--$COMPILE_MODULES_FLAG` with a runtime setting.
+Override the `julia` command line flag `--$COMPILED_MODULES_FLAG` with a runtime setting.
 Code run before this the value is modified will use the original setting. Not meant for
 general purpose usage.
 """
-function use_precompile(state::Bool)
+function set_compiled_modules(state::Bool)
     value = Base.convert(Int8, state)
 
     # Load the C global into a mutable Julia type
@@ -51,9 +57,9 @@ function use_precompile(state::Bool)
     opts = unsafe_load(jl_options)
 
     # Avoid modifying the global when the value hasn't changed
-    if getfield(opts, COMPILE_MODULES_FIELD) != value
+    if getfield(opts, COMPILED_MODULES_FIELD) != value
         warn("Using experimental code which modifies jl_options global struct")
-        setfield!(opts, COMPILE_MODULES_FIELD, value)
+        setfield!(opts, COMPILED_MODULES_FIELD, value)
         unsafe_store!(jl_options, opts)
     end
 
