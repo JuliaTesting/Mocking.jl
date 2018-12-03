75d4837ab042345089ff6206bcd0fb1434bc17d9
Lyndon White <lyndon.white@invenialabs.co.uk>
2018-12-03 13:53:58 -0600

    fix @mock noop maxlog

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,11 +1,11 @@
 import Base: @deprecate_binding, @deprecate
 
 macro mock(expr)
-    @warn "@mock is deprecated to nothing. It is no longer required" max_log=1
+    @warn "@mock is deprecated to nothing. It is no longer required" maxlog=1
     return esc(expr)
 end
 
 function enable(;force::Bool=false)
-    @warn "enable is deprecated to nothing. It is no longer required" max_log=1
+    @warn "enable is deprecated to nothing. It is no longer required" maxlog=1
     return
 end
