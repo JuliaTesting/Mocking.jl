b4b8b1399d27d581d9d36f84d28550d1fa5b793c
Lyndon White <lyndon.white@invenialabs.co.uk>
2018-12-06 12:42:42 -0600

    allow readme test to fail

--- test/readme.jl
+++ test/readme.jl
@@ -17,18 +17,18 @@ using Base: Sys, read, unsafe_string
     end
 
     # Produces a string with sequential UInt8 values from 1:n
-    data = unsafe_string(pointer(convert(Array{UInt8}, 1:n)))
+    data = String(UInt8.(1:n))
 
     # Generate a alternative method of `open` which call we wish to mock
     patch = @patch open(fn::Function, f::AbstractString) = fn(IOBuffer(data))
 
     # Apply the patch which will modify the behaviour for our test
     apply(patch) do
-        @test randdev(n) == convert(Array{UInt8}, n:-1:1)
+        @test_broken randdev(n) == UInt8.(n:-1:1) # https://github.com/jrevels/Cassette.jl/issues/89
     end
 
     if Sys.isunix()
         # Outside of the scope of the patched environment `@mock` is essentially a no-op
-        @test randdev(n) != convert(Array{UInt8}, n:-1:1)
+        @test randdev(n) != UInt8.(n:-1:1) # Very unlikely
     end
 end
