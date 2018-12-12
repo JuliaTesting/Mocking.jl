a3a09a69510ea4434a7fa590db43754e76d6ea59
Lyndon White <lyndon.white@invenialabs.co.uk>
2018-12-12 14:19:06 -0600

    fix typo in test

--- test/deprecations.jl
+++ test/deprecations.jl
@@ -2,4 +2,4 @@
 @test_logs (:warn, r"can be removed") (@mock identity(1))
 
 @test_logs (:warn, r"can be removed") (Mocking.enable())
-@test_logs (:warn, r"no can be removed") (Mocking.enable(; force=true))
+@test_logs (:warn, r"can be removed") (Mocking.enable(; force=true))
