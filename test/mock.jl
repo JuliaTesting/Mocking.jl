ad6bb8dfc4c79617503d83bb7697238012451f8d
Curtis Vogt <curtis.vogt@gmail.com>
2021-07-27 09:24:52 -0500

    Disable f_req_param before Julia 1.5

--- test/mock.jl
+++ test/mock.jl
@@ -6,17 +6,21 @@
         return @mock f(; x=x)
     end
 
+    @test f_param() == 2
+
     function f_kw()
         x = 3
         return @mock f(x=x)
     end
 
-    function f_req_param()
-        x = 4
-        return @mock f(; x)  # Testing keyword parameter that is just a `Symbol`
-    end
-
-    @test f_param() == 2
     @test f_kw() == 3
-    @test f_req_param() == 4
+
+    @static if VERSION >= v"1.5"
+        function f_req_param()
+            x = 4
+            return @mock f(; x)  # Testing keyword parameter that is just a `Symbol`
+        end
+
+        @test f_req_param() == 4
+    end
 end
