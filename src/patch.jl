6c11e5dd438fb208449f0f0ae456d6b784bdab73
Curtis Vogt <curtis.vogt@gmail.com>
2021-10-06 09:53:45 -0500

    Use ContextVariablesX to fix async issue

--- src/patch.jl
+++ src/patch.jl
@@ -126,19 +126,14 @@ end
 ```
 """
 function apply(body::Function, pe::PatchEnv)
-    prev_pe = get_active_env()
-    set_active_env(merge(prev_pe, pe))
-    try
-        return body()
-    finally
-        set_active_env(prev_pe)
-    end
+    merged_pe = merge(patch_env[], pe)
+    return with_context(body, patch_env => merged_pe)
 end
 
 function apply(body::Function, patches; debug::Bool=false)
     return apply(body, PatchEnv(patches, debug))
 end
 
-const PATCH_ENV = Ref{PatchEnv}(PatchEnv())
-set_active_env(pe::PatchEnv) = (PATCH_ENV[] = pe)
-get_active_env() = PATCH_ENV[]
+@contextvar patch_env = PatchEnv()
+set_active_env(body::Function, pe::PatchEnv) = with_context(body, patch_env => pe)
+get_active_env() = patch_env[]
--- test/runtests.jl
+++ test/runtests.jl
@@ -28,4 +28,5 @@ using Mocking: anon_morespecific, anonymous_signature, dispatch, type_morespecif
     include("args.jl")
     include("merge.jl")
     include("nested_apply.jl")
+    include("async.jl")
 end
