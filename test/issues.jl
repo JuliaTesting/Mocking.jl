791ecf8acc9291b84589db3b32963127927c7319
Curtis Vogt <curtis.vogt@gmail.com>
2021-10-14 11:13:00 -0500

    Add ContextVariablesX issue test

---
+++ test/issues.jl
@@ -0,0 +1,9 @@
+# https://github.com/invenia/Mocking.jl/issues/92
+@testset "ContextVariableX logging incompatibility" begin
+    generate_report() = "original"
+    report_patch = @patch generate_report() = "mocked"
+
+    apply(report_patch) do
+        @test (@test_logs @mock generate_report()) == "mocked"
+    end
+end
